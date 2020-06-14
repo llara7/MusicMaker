297ac9c0d2157a7ec001c321a446a6869d2ce7e6
llara7 <lllaralaura7@gmail.com>
2020-06-14 12:26:09 +0000

    small tweaks

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
- 
+  const tads = document.querySelectorAll(".tads div");
+
+  tads.forEach((tad, index) =>{
+    tad.addEventListener('click', function(){
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+    });
+  });
   pads.forEach((pad,index)  =>{
     pad.addEventListener('click', function(){
       sounds[index].currentTime = 0;
